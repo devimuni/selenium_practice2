53bf148a43b32aa0a0f492dc20855169a6433fb5
devimuni <devi.muni@gmail.com>
2020-08-10 18:07:20 +0530

    adding to github

---
+++ run.bat
@@ -0,0 +1,3 @@
+d:
+cd D:\Selenium\selenium_practice2
+mvn clean install
---
+++ src/test/java/basicsanity/Basicsanity.java
@@ -0,0 +1,62 @@
+package basicsanity;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basicsanity {
+
+
+	public static Logger logger = LogManager.getLogger(Basicsanity.class);
+
+	public WebDriver driver;
+
+	@Parameters("browsername")
+	@Test
+	public void launchapplication(String browsername)
+	{
+		if(browsername.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver= new ChromeDriver();
+			driver.get("https://example.testproject.io/web/");
+			
+			//logger.error("testproject application launched");
+			System.out.println("Successfully launched in chrome and title is  " + driver.getTitle());
+			driver.quit();
+		}
+		else if(browsername.equalsIgnoreCase("IE"))
+		{
+			WebDriverManager.iedriver().setup();	
+			driver= new InternetExplorerDriver();
+			driver.get("https://example.testproject.io/web/");
+		
+			System.out.println("Successfully launched in IE and title is   " +  driver.getTitle());
+			driver.quit();
+		}
+		else if(browsername.equalsIgnoreCase("Firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get("https://example.testproject.io/web/");
+			
+			System.out.println("Successfully launched in firefox and title is   " + driver.getTitle());
+			driver.quit();
+			
+		}
+	}
+
+
+}
+
